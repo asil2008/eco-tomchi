e5aa2e98657d71edda3cc5972e02ffd4b3bde33f
sobitvc <toshtemigofurov@gmail.com>
2025-12-17 14:51:07 +0500

    Added image_url field and updated templates

--- articles/models.py
+++ articles/models.py
@@ -1,29 +1,23 @@
 from django.db import models
-from django.utils.text import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-
-    class Meta:
-        verbose_name_plural = "Categories"
 
     def __str__(self):
         return self.name
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True, blank=True)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-    featured_image = models.ImageField(upload_to='articles/%Y/%m/%d/')
+    slug = models.SlugField(unique=True)
     content = models.TextField()
+    # Fayl yuklash uchun (ixtiyoriy)
+    featured_image = models.ImageField(upload_to='articles/', blank=True, null=True)
+    # INTERNETDAN LINK QO'YISH UCHUN (ASOSIY):
+    image_url = models.URLField(max_length=500, blank=True, null=True)
+    
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='articles')
     published_date = models.DateTimeField(auto_now_add=True)
-    is_daily_highlight = models.BooleanField(default=False)
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
